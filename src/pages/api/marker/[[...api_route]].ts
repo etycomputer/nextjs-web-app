8daf1eabc1a0f1493d18a26c7d1c072a97a5105f
Taha Amiri <amiri.taha.98@gmail.com>
2023-05-14 12:26:42 -0700

    [ADD] api handler for markers list

--- src/pages/api/marker/[[...api_route]].ts
+++ src/pages/api/marker/[[...api_route]].ts
@@ -1,3 +1,9 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+export type ErrorResponse = {
+  message: string;
+};
+
 export type MarkerResponse = {
   markerId: number;
   subnet: number;
@@ -100,3 +106,128 @@ let ReadingList: ReadingListResponse = [
     porePressure: 50100.0,
   },
 ];
+
+export default async function handler (
+  req: NextApiRequest,
+  res: NextApiResponse<MarkerResponse | MarkerListResponse | ErrorResponse>
+) {
+  try {
+    const api_query = req.query;
+    if ('api_route' in api_query) {
+      const api_route = api_query['api_route'] ?? [];
+      let route_index = 0;
+      const current_route = api_route[route_index++] ?? '';
+      if (current_route.toLowerCase() === 'list') {
+        // GET /api/marker/list
+        // GET Get all marker.
+        res.status(200).json(MarkerList);
+        return;
+      }
+
+      const marker_id = Number.parseInt(current_route);
+      if (Number.isNaN(marker_id) || marker_id <= 0) {
+        res.status(400).json({
+          message: 'Invalid request parameter.',
+        });
+        return;
+      }
+
+      if (req.method === 'GET') {
+        // GET /api/marker/{marker_id}
+        // GET Get an marker by its ID.
+        const markerById = MarkerList.find(
+          (obj) => obj.markerId === marker_id
+        );
+        if (markerById === undefined) {
+          res.status(404).json({
+            message: 'Object not found.',
+          });
+          return;
+        }
+        res.status(200).json(markerById);
+        return;
+      } else if (req.method === 'PUT') {
+        // PUT /api/marker/{marker_id}
+        // PUT Update an object by its ID.
+        const body = req.body;
+        if (!body || typeof body !== 'object') {
+          res.status(400).json({
+            message: 'Request body is required.',
+          });
+          return;
+        }
+        const { activated, activationTime, node, subnet } = req.body as UpdateMarkerResponse;
+        if (
+          !Number.isInteger(subnet) ||
+          !Number.isInteger(node) ||
+          typeof activated !== 'boolean' ||
+          (activationTime !== null && !(activationTime instanceof Date))
+        ) {
+          res.status(400).json({
+            message: 'Invalid request body.',
+          });
+          return;
+        }
+        const marker_index = MarkerList.findIndex(
+          (obj) => obj.markerId === marker_id
+        );
+        if (marker_index === -1) {
+          res.status(404).json({
+            message: 'Object not found.',
+          });
+          return;
+        }
+        const updatedObject = {
+          ...MarkerList[marker_index],
+          ...body,
+          objectsId: marker_id,
+        };
+        MarkerList[marker_index] = updatedObject;
+        res.status(200).json(updatedObject);
+        return;
+      } else {
+        res.status(405).json({
+          message: 'Invalid request method.',
+        });
+      }
+    } else if (req.method === 'POST') {
+      // POST /api/marker
+      // POST Add a new marker.
+      const body = req.body;
+      if (!body || typeof body !== 'object') {
+        res.status(400).json({
+          message: 'Request body is required.',
+        });
+        return;
+      }
+      const { activated, activationTime, node, subnet } = req.body as UpdateMarkerResponse;
+      if (
+        !Number.isInteger(subnet) ||
+        !Number.isInteger(node) ||
+        typeof activated !== 'boolean' ||
+        (activationTime !== null && !(activationTime instanceof Date))
+      ) {
+        res.status(400).json({
+          message: 'Invalid request body.',
+        });
+        return;
+      }
+      const object_index = MarkerList.push({
+        markerId: MarkerList.length + 1,
+        activated: activated,
+        activationTime: activationTime,
+        node: node,
+        subnet: subnet
+      });
+      res.status(200).json(MarkerList[object_index]);
+    } else {
+      res.status(405).json({
+        message: 'Invalid request method.',
+      });
+    }
+  } catch (err) {
+    res.status(500).json({
+      message: 'failed to load data',
+    });
+  }
+}
