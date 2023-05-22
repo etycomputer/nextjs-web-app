935ad427c352c03b4c40e14128dad9d956fcfbc3
amiriTaha <amiri.taha.98@gmail.com>
2023-05-22 10:19:42 +0330

    [ADD] return onHover for hovered marker and show tooltip

--- src/utils/components/threeObjectViewer/Sphere.tsx
+++ src/utils/components/threeObjectViewer/Sphere.tsx
@@ -1,14 +1,14 @@
 import { setMarker } from '@/redux/features/markerSlice';
 import { useAppDispatch, useAppSelector } from '@/redux/hooks';
-import { ThreeElements } from '@react-three/fiber';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 
 interface SphereProps {
 	marker: any;
+	onHover: (marker: any) => void;
 }
 
-export const Sphere = ({ marker }: SphereProps) => {
+export const Sphere = ({ marker, onHover }: SphereProps) => {
 
 	const dispatch = useAppDispatch();
 	const selectedMarker = useAppSelector((state) => state.markerReducer.marker);
@@ -20,16 +20,22 @@ export const Sphere = ({ marker }: SphereProps) => {
 		dispatch(setMarker(marker));
 	}
 
+	useEffect(() => {
+		onHover(hovered ? marker : undefined);
+	}, [hovered])
+
 	return (
-		<mesh
-			scale={0.5}
-			position={[marker.x, marker.y, marker.z]}
-			onClick={markerClickHandler}
-			onPointerOver={() => setHover(true)}
-			onPointerOut={() => setHover(false)}
-		>
-			<sphereBufferGeometry args={[1, 64, 64]} />
-			<meshStandardMaterial color={hovered || selectedMarker?.positionId === marker.positionId ? 'hotpink' : 'orange'} />
-		</mesh>
+		<>
+			<mesh
+				scale={0.5}
+				position={[marker.x, marker.y, marker.z]}
+				onClick={markerClickHandler}
+				onPointerOver={() => setHover(true)}
+				onPointerOut={() => setHover(false)}
+			>
+				<sphereBufferGeometry args={[1, 64, 64]} />
+				<meshStandardMaterial color={hovered || selectedMarker?.positionId === marker.positionId ? 'hotpink' : 'orange'} />
+			</mesh>
+		</>
 	);
 };
