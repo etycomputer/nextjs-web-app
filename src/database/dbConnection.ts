6e534d50209d10e25dcf1357dd6981de7e0a7c59
Ehsan Tabatabaei Yazdi <ehsan.yazdi@elexonmining.com>
2023-05-27 01:59:25 +1000

    Added DB connection

---
+++ src/database/dbConnection.ts
@@ -0,0 +1,23 @@
+import { DataSource } from 'typeorm';
+
+export const DB_Connection = new DataSource({
+    type: 'postgres',
+    name: 'default',
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT),
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    synchronize: false,
+    logging: false,
+    entities: ['src/entities/**/*.ts'],
+});
+
+DB_Connection.initialize()
+    .then(() => {
+        console.log("Data Source has been initialized!")
+    })
+    .catch((err) => {
+        console.error("Error during Data Source initialization", err)
+    })
+
